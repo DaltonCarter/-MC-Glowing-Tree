b08352d7fcea219c001f06d6493f6ca7979bfac9
Dalton Carter <Dj_Carter_Gottenbos@yahoo.com>
2023-10-15 16:26:21 -0700

    Glowing Tree Started

---
+++ src/main/java/com/CartersDev/GlowingTree/item/ModItems.java
@@ -0,0 +1,29 @@
+package com.CartersDev.GlowingTree.item;
+
+import com.CartersDev.GlowingTree.Tree;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Tree.MOD_ID);
+
+
+    public static final RegistryObject<Item> GLOWINGOAK = ITEMS.register("glowingoak",
+            () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
+
+
+
+
+
+
+    public static void register(IEventBus eventBus) {
+
+        ITEMS.register(eventBus);
+    }
+
+}
